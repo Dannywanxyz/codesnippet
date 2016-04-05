ee832261bfd24c02caef73545564065818b876a8
David Wan <david@google.com>
2016-04-05 15:19:38 +0800

    对象拷贝

---
+++ obj_copy.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env/ python
+# coding=utf-8
+# 深拷贝与浅拷贝
+import copy
+a=[[1],[2],[3]]
+b=copy.copy(a)
+c=copy.deepcopy(a)
+print "before=>"
+print a
+print b
+print c
+a[0][0] = 0
+a[1]=None
+print 'after=>'
+print a
+print b
+print c
